60c13427d5c233fe3e9dc250417bdb7600e53979
arakiyo <yota.arakip@normal-inc.com>
2025-12-26 08:15:29 +0000

    env, zod

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@/lib/zod/env";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
